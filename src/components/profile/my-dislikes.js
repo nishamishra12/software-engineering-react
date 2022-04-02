2105cee2840e8517ade9a6c654c84f57024ac508
nishamishra12 <nishamishra093@gmail.com>
2022-04-02 13:06:19 -0400

    Dislike Tuit
    Added Dislike Tuit Functionality - Profile, service, and icons

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,8 +1,19 @@
-const MyDislikes = () => {
+import Tuits from "../tuits";
+import * as service from "../../services/dislikes-service";
+import {useEffect, useState} from "react";
+
+const MyDisLikes = () => {
+    const [dislikedTuits, setDisLikedTuits] = useState([]);
+    const findTuitsIDisLike = () =>
+        service.findAllTuitsDisLikedByUser("me")
+            .then((tuits) => setDisLikedTuits(tuits));
+    useEffect(findTuitsIDisLike, []);
+    
     return(
         <div>
-            <h1>My Dislikes</h1>
+            <h2>My Dis-Likes</h2>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDisLike}/>
         </div>
-    )
-}
-export default MyDislikes;
+    );
+};
+export default MyDisLikes;
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit = () => {}, dislikeTuit =() => {}}) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -26,6 +26,20 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
                   <i className="fa-solid fa-thumbs-up"></i>
               }
             <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
+          </span>
+        </div>
+
+        <div className="col">
+          <span className="ttr-dislike-tuit-click" onClick={() => dislikeTuit(tuit)}>
+              {
+                tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
+                  <i className="fa-solid fa-thumbs-down" style={{color: 'black'}}></i>
+              }
+              {
+                tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes <= 0 &&
+                  <i className="fa-solid fa-thumbs-down"></i>
+              }
+            <span className="ttr-stats-dislikes">{tuit.stats && tuit.stats.dislikes}</span>
           </span>
         </div>
         <div className="col">
